3efa32ca0500d70c330839adc9f75564d64614ad
Afatwist <afatwist@yandex.ru>
2024-12-09 22:10:23 +0300

    Models cleaning

--- scripts/Classes/Models/LootModel.js
+++ scripts/Classes/Models/LootModel.js
@@ -26,53 +26,9 @@ export class LootModel extends FallingModel {
     fallDown() {
         // проверка, что предмет все еще существует
         if (!this.element || !LootList.getOne(this.id)) {
-            console.error(this, LootList.getOne(this.id));
+            console.error('такого лута уже нет', this, LootList.getOne(this.id));
             return;
         }
         super.fallDown();
-    }
-
-
-
-    /** Падение предмета
-     * 
-     * @returns {void}
-     */
-    fallDownQQ() { // !!! старый метод
-        // Предмет падает в клетку под собой, если она пустая. 
-        // Если клетка с типом земля или стена, то предмет останавливается в текущей клетке.
-        // Если в клетке другой предмет, то проверяются боковые клетки от предмета, 
-        // если они свободны, предмет может скатиться вниз по диагонали.
-
-        // проверка, что предмет все еще существует
-        if (!this.element || !LootList.getOne(this.id)) {
-            console.error(this, LootList.getOne(this.id));
-            return;
-        }
-
-        let { row, col } = this.getCoordinates();
-
-        // клетка под предметом, для падения вниз 
-        let cellBottom = CellList.getOne(row + 1, col);
-
-        if (!cellBottom) {
-            this.fall = 0;
-            return;
-        }
-
-
-        let rightBottom = this.rightSideCell(row, col);
-
-        let leftBottom = this.leftSideCell(row, col);
-
-        // предмет лежит на другом предмете, т.е в клетке под предметом что-то есть, но не игрок
-        let hasCellBottomItem = cellBottom.itemHas() && !cellBottom.hasPlayer();
-
-        // падение предмета
-        if (cellBottom.isFree()) this.fallStep(cellBottom);
-        else if (rightBottom && hasCellBottomItem) this.fallStep(rightBottom);
-        else if (leftBottom && hasCellBottomItem) this.fallStep(leftBottom);
-        else this.fall = 0;
-
     }
 }
--- scripts/Classes/Models/StoneModel.js
+++ scripts/Classes/Models/StoneModel.js
@@ -27,64 +27,9 @@ export class StoneModel extends FallingModel {
     fallDown() {
         // проверка, что предмет все еще существует
         if (!this.element || !StoneList.getOne(this.id)) {
-            console.error(this, StoneList.getOne(this.id));
+            console.error('Такого камня уже нет', this, StoneList.getOne(this.id));
             return;
         }
         super.fallDown();
-    }
-
-    /** Падение предметов */ //!!! Старый метод
-    fallDownQQ() {
-        // Предмет падает в клетку под собой, если она пустая. 
-        // Если клетка с типом земля или стена, то предмет останавливается в текущей клетке.
-        // Если в летке враг или игрок, предмет убивает его, при условии, что высота падения более 1 клетки. (dataset.fall)
-        // Если в клетке другой предмет, то проверяются боковые клетки от предмета, если они свободны, предмет может скатиться вниз по диагонали.
-
-        // проверка, что предмет все еще существует
-        if (!this.element || !StoneList.getOne(this.id)) {
-            console.error(this, StoneList.getOne(this.id));
-            return;
-        }
-
-        let { row, col } = this.getCoordinates();
-
-        // клетка под предметом, для падения вниз 
-        let cellBottom = CellList.getOne(row + 1, col);
-
-        if (!cellBottom) {
-            this.fall = 0;
-            return;
-        }
-
-
-        if (cellBottom.itemHas() && this.fall > 1) {
-            let item = cellBottom.itemGet();
-            if (item === Player) {
-                Player.die();
-                return;
-            }
-            if (item instanceof EnemyModel) {
-                item.die();
-                return;
-            }
-        }
-
-
-
-
-        let rightBottom = this.rightSideCell(row, col);
-
-        let leftBottom = this.leftSideCell(row, col);
-
-        // предмет лежит на другом предмете, т.е в клетке под предметом что-то есть, но не игрок
-        let hasCellBottomItem = cellBottom.itemHas() && !cellBottom.hasPlayer();
-
-        // падение предмета
-        if (cellBottom.isFree()) this.fallStep(cellBottom);
-        else if (rightBottom && hasCellBottomItem) this.fallStep(rightBottom);
-        else if (leftBottom && hasCellBottomItem) this.fallStep(leftBottom);
-        else this.fall = 0;
-
-
     }
 }
